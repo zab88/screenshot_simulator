6e8e3a7e689f51f36bfcf8e3cddf91be700ef74f
zab88 <alexey.zabaykin@gmail.com>
2016-11-03 11:41:33 +0700

    bug with cursor out of border fixed

--- screenshot_simulator.py
+++ screenshot_simulator.py
@@ -9,7 +9,7 @@ def f1(bg, mouse, location, filename):
     :param mouse: mage of a mouse pointer (OpenCV image)
     :param location: tuple (x,y) in percentages of background image width/height
     :param filename: Filename where to save the result image (string)
-    :return:
+    :return: OpenCV image
     """
     rows, cols, channels = mouse.shape
     if channels != 4:
@@ -22,14 +22,23 @@ def f1(bg, mouse, location, filename):
     roi = bg[y:y + rows, x:x + cols]
 
     # Now create a mask of cursor and create its inverse mask also
-    mask = mouse[:, :, 3]
+    if channels == 4:
+        mask = np.array(mouse[:, :, 3], np.uint8)
+    else:
+        mask = np.zeros((rows, cols), np.uint8)
+
     mouse = cv2.merge([mouse[:, :, 0], mouse[:, :, 1], mouse[:, :, 2]])
     mask_inv = cv2.bitwise_not(mask)
 
-    # Now black-out the area of logo in ROI
+    # if mouse out of bounds, mask should be contracted
+    mask = mask[:roi.shape[0], :roi.shape[1]]
+    mask_inv = mask_inv[:roi.shape[0], :roi.shape[1]]
+    mouse = mouse[:roi.shape[0], :roi.shape[1]]
+
+    # Now black-out the area of cursor in ROI
     img1_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
 
-    # Take only region of logo from logo image.
+    # Take only region of cursor from cursor image.
     img2_fg = cv2.bitwise_and(mouse, mouse, mask=mask)
 
     # Put logo in ROI and modify the main image
@@ -42,18 +51,26 @@ def f1(bg, mouse, location, filename):
     return bg
 
 
-def f2(cv_img, scale_params):
+def f2(cv_img, scale_params, transparent_bg=True):
     """
     :param cv_img: opencv image
     :param scale_params: tuple height/width/rotation degree
     :return: image, matrix
     """
-    rows, cols = cv_img.shape[:2]
+    rows, cols, channels = cv_img.shape
 
     # first - resize width and height
-    fy = (float(scale_params[0]) / rows)
-    fx = (float(scale_params[1]) / cols)
-    res = cv2.resize(cv_img, None, fx=fx, fy=fy, interpolation=cv2.INTER_AREA)  # INTER_AREA is faster then INTER_CUBIC
+    # if new width or height is None, then no resize
+    if (scale_params[0] is None) or (scale_params[1] is None):
+        res = cv_img
+    else:
+        fy = (float(scale_params[0]) / rows)
+        fx = (float(scale_params[1]) / cols)
+        res = cv2.resize(cv_img, None, fx=fx, fy=fy, interpolation=cv2.INTER_CUBIC)  # INTER_AREA is faster then INTER_CUBIC
+
+    # if no rotation
+    if not scale_params[2]:
+        return res, res[:,:,3] if channels == 4 else np.zeros((rows, cols), np.uint8)
 
     # to avoid missing corners after rotation - image should be expanded
     rows, cols = res.shape[:2]
@@ -66,7 +83,7 @@ def f2(cv_img, scale_params):
 
     # mask creating
     mask_ = np.zeros((rows_ext, cols_ext), np.uint8)
-    cv2.rectangle(mask_, (lr_border, tb_border), (cols_ext - lr_border, rows_ext - tb_border), 255, cv2.FILLED)
+    cv2.rectangle(mask_, (lr_border, tb_border), (cols_ext - lr_border, rows_ext - tb_border), 255, -1)
 
     # second - apply rotation
     # rotation matrix creation
@@ -74,6 +91,11 @@ def f2(cv_img, scale_params):
 
     # affine transform
     dst = cv2.warpAffine(img_ext, M, (cols_ext, rows_ext))  # borderMode=cv2.BORDER_TRANSPARENT, borderValue=0
-    mask_ = cv2.warpAffine(mask_, M, (cols_ext, rows_ext))
+
+    # mask can be not rectangle, if input image was with transparency
+    if dst.shape[2] == 4:
+        mask_ = dst[:,:,3]
+    else:
+        mask_ = cv2.warpAffine(mask_, M, (cols_ext, rows_ext))
 
     return dst, mask_
